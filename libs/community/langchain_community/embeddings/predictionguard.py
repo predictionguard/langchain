746cc7261869db0f3c0de000bbfa897ac06b8a5d
jmansdorfer <jmansdorfer19@gmail.com>
2024-08-08 11:35:53 -0400

    embeddings tests and notebook done.

--- libs/community/langchain_community/embeddings/predictionguard.py
+++ libs/community/langchain_community/embeddings/predictionguard.py
@@ -1,17 +1,17 @@
 import logging
 from typing import Any, Dict, List, Optional
+import base64
 
 from langchain_core.callbacks import CallbackManagerForLLMRun
-from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.embeddings import Embeddings
 from langchain_core.pydantic_v1 import Extra, root_validator
-from langchain_core.utils import get_from_dict_or_env
+from langchain_core.utils import get_from_dict_or_env, convert_to_secret_str
 
-from langchain_community.
 
 logger = logging.getLogger(__name__)
 
 
-class PredictionGuardEmbeddings(BaseChatModel):
+class PredictionGuardEmbeddings(Embeddings):
     """Prediction Guard chat models.
     
     To use, you should have the ``predictionguard`` python package installed, and the
@@ -21,7 +21,8 @@ class PredictionGuardEmbeddings(BaseChatModel):
     Example:
         .. code-block:: python
         
-            embeddings = PredictionGuard(model="bridgetower-large-itm-mlm-itc",
+            embeddings = PredictionGuardEmbeddings(
+                                    model="bridgetower-large-itm-mlm-itc",
                                     api_key="my-api-key"
                                     )
     """
@@ -30,25 +31,155 @@ class PredictionGuardEmbeddings(BaseChatModel):
     model: Optional[str] = "bridgetower-large-itm-mlm-itc"
     """Model name to use."""
 
-    input: Optional[Dict[str, Any]] = None
-    """The input check to run over the prompt before sending to the LLM."""
-
-    output: Optional[Dict[str, Any]] = None
-    """The output check to run the LLM output against."""
-
-    max_tokens: int = 256
-    """Denotes the number of tokens to predict per generation."""
-
-    temperature: float = 0.75
-    """A non-negative float that tunes the degree of randomness in generation."""
-
-    top_p: float = 0.1
-    """A non-negative float that controls the diversity of the generated tokens."""
-
     api_key: Optional[str] = None
     """Your Prediction Guard api_key."""
 
-    stop: Optional[List[str]] = None
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        """Validate that api key and python package exists in environment."""
+        values["predictionguard_api_key"] = convert_to_secret_str(
+            get_from_dict_or_env(values, "predictionguard_api_key", "PREDICTIONGUARD_API_KEY")
+        )
+
+        try:
+            from predictionguard import PredictionGuard
+
+        except ImportError as e:
+            raise ImportError(
+                "Could not import predictionguard python package. "
+                "Please install it with `pip install predictionguard --upgrade`."
+            ) from e
+
+        client = PredictionGuard(
+            api_key=values["predictionguard_api_key"].get_secret_value(),
+        )
+
+        values["client"] = client
+        return values
+    
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        """Call out to Prediction Guard's embedding endpoint for embedding documents.
+        
+        Args:
+            texts:
+                List of dictionaries containing text inputs.
+            
+        Returns:
+            Embeddings for the texts.
+        """
+
+        inputs = []
+        for text in texts:
+            input = {
+                "text": text
+            }
+
+            inputs.append(input)
+
+        response = self.client.embeddings.create(
+            model=self.model,
+            input=inputs
+        )
+
+        res = []
+        indx = 0
+        for re in response["data"]:
+            if re["index"] == indx:
+                res.append()
+                indx += 1
+            else:
+                continue
+
+        return res
+
+
+    def embed_query(self, text: str) -> List[float]:
+        """Call out to Prediction Guard's embedding endpoint for embedding query text.
+
+        Args:
+            text: The text to embed.
+
+        Returns:
+            Embeddings for the text.
+        """
+
+        inputs = [
+            {
+                "text": text
+            }
+        ]
+
+        response = self.client.embeddings.create(
+            model=self.model,
+            input=inputs
+        )
+
+        return response["data"][0]["embedding"]
+    
+
+    def embed_images(self, images: List[str]) -> List[float]:
+        """Call out to Prediction Guard's embedding endpoint for embedding multiple images.
+        
+        Args:
+            images: A list of images to embed. Supports image file paths, image URLs, data URIs, and base64 encoded images.
+            
+        Returns:
+            Embeddings for the images.
+        """
+
+        inputs = []
+        for image in images:
+            input = {
+                "image": image
+            }
+
+            inputs.append(input)
+
+        response = self.client.embeddings.create(
+            model=self.model,
+            input=inputs
+        )
+
+        res = []
+        indx = 0
+        for re in response["data"]:
+            if re["index"] == indx:
+                res.append()
+                indx += 1
+            else:
+                continue
+
+        return res
+
+
+    def embed_image_text(self, inputs: List[Dict[str, str]]) -> List[float]:
+        """Call out to Prediction Guard's embedding endpoint for embedding an image and text.
+        
+        Args:
+            inputs: A list of dictionaries containing the text and images to embed.
+            
+        Returns:
+            Embeddings for the text and images.
+        """
+
+        response = self.client.embeddings.create(
+            model=self.model,
+            input=inputs
+        )
+
+        res = []
+        indx = 0
+        for re in response["data"]:
+            if re["index"] == indx:
+                res.append()
+                indx += 1
+            else:
+                continue
+
+        return res
+
 
     class Config:
         """Configuration for this pydantic object."""
--- libs/community/tests/integration_tests/embeddings/test_predictionguard.py
+++ libs/community/tests/integration_tests/embeddings/test_predictionguard.py
@@ -4,11 +4,83 @@
 from langchain_community.embeddings.predictionguard import PredictionGuardEmbeddings
 
 
-def test_predictionguard_call() -> None:
-    """Test a valid call to Prediction Guard."""
-    embeddings = PredictionGuardEmbeddings(model="bridgetower-large-itm-mlm-itc")
+def test_predictionguard_embeddings_documents() -> None:
+    """Test Prediction Guard embeddings."""
+    embeddings = PredictionGuardEmbeddings()
+    documents = ["embed this"]
+    output = embeddings.embed_documents(documents)
+    assert len(output) == 1
+    assert len(output[0]) > 2
 
-    text = "This text is for testing."
-    query_result = embeddings.embed_query(text)
 
-    assert isinstance(query_result[0], float)
+def test_predictionguard_embeddings_documents_multiple() -> None:
+    """Test Prediction Guard embeddings."""
+    embeddings = PredictionGuardEmbeddings()
+    documents = ["embed me", "embed this"]
+    output = embeddings.embed_documents(documents)
+    assert len(output) == 2
+    assert len(output[0]) > 2
+    assert len(output[1]) > 2
+
+
+def test_predictionguard_embeddings_query() -> None:
+    """Test Prediction Guard embeddings."""
+    embeddings = PredictionGuardEmbeddings()
+    document = "embed this"
+    output = embeddings.embed_query(document)
+    assert len(output) > 2
+
+
+def test_predictionguard_embeddings_images() -> None:
+    """Test Prediction Guard embeddings."""
+    embeddings = PredictionGuardEmbeddings()
+    image = "https://farm4.staticflickr.com/3300/3497460990_11dfb95dd1_z.jpg"
+    output = embeddings.embed_images(image)
+    assert len(output) == 1
+    assert len(output[0]) > 2
+
+
+def test_predictionguard_embeddings_images_multiple() -> None:
+    """Test Prediction Guard embeddings."""
+    embeddings = PredictionGuardEmbeddings()
+    images = [
+        "https://farm4.staticflickr.com/3300/3497460990_11dfb95dd1_z.jpg", 
+        "https://pbs.twimg.com/media/GKLN4qPXEAArqoK.png"
+        ]
+    output = embeddings.embed_images(images)
+    assert len(output) == 2
+    assert len(output[0]) > 2
+    assert len(output[1]) > 2
+
+
+def test_predictionguard_embeddings_image_text() -> None:
+    """Test Prediction Guard Embeddings"""
+    embeddings = PredictionGuardEmbeddings()
+    inputs = [
+        {
+            "text": "embed me",
+            "image": "https://pbs.twimg.com/media/GKLN4qPXEAArqoK.png"
+        }
+    ]
+    output = embeddings.embed_image_text(inputs)
+    assert len(output) == 1
+    assert len(output[0]) > 2 
+
+
+def test_predictionguard_embeddings_image_text_multiple() -> None:
+    """Test Prediction Guard Embeddings"""
+    embeddings = PredictionGuardEmbeddings()
+    inputs = [
+        {
+            "text": "embed me",
+            "image": "https://pbs.twimg.com/media/GKLN4qPXEAArqoK.png"
+        },
+        {
+            "text": "embed this",
+            "image": "https://farm4.staticflickr.com/3300/3497460990_11dfb95dd1_z.jpg"
+        }
+    ]
+    output = embeddings.embed_image_text(inputs)
+    assert len(output) == 2
+    assert len(output[0]) > 2 
+    assert len(output[1]) > 2
